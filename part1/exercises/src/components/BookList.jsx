5844f99e57e7b34d251571abb6e759b48f2ba063
alexdavies440 <alexdavies440@gmail.com>
2024-08-18 21:25:22 -0500

    Added border and margins

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import classes from "./BookList.module.css"
+
 export default function BookList() {
    let pageTitle = "Book List";
    let book1 = "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRgzcGPhKfYv9J9M9COOlYJRZ7XB84ZIcENU2uAKXBr615UeyL7unyPKf6TkuiOm7KvkUGsnjEXGZ623u2124hrWSO3hQTVP8J4XGe6AA&usqp=CAc";
@@ -5,7 +8,7 @@ export default function BookList() {
    let book3 = "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQAmZzkXex7niJFKNomtPArpBIX9CRFzIJHcTe4UJgJZ-9EBbw3dlWYPA0e_gEKoqhkFQGdDoSVS4BVPR-s3MxGUMWEwXrH-DLn4nG-npw&usqp=CAc";
 
    return (
-      <div>
+      <div className={classes.bookList}>
          <h3>{pageTitle}</h3>
          <img src={book1} alt="Unmasking Autism by Devon Price" />
          <img src={book2} alt="The Reckoning by John Grisham" />
