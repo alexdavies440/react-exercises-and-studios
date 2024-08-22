80f4be79cd241553a1ef7001b8e53669e68602de
alexdavies440 <alexdavies440@gmail.com>
2024-08-22 00:05:27 -0500

    Hitting a wall on listItem function, will come back to this tomorrow

--- part2/exercises-2/src/components/Button.jsx
+++ part2/exercises-2/src/components/Button.jsx
@@ -1,9 +1,11 @@
 import './styles.css';
 
 function Button() {
-
+   const onLearnMore = () => {
+         alert("Save the whales!");
+   }
    return ( 
-         <button>
+         <button onClick={onLearnMore}>
             Learn More
          </button>
    );
--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,12 +1,20 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json'
 
 
 function Profile()
 {
+   const listItem = oceans.map((ocean, id) => 
+   <div key={id}>
+      <img src={ocean.image} alt={ocean.name} className='img'/>
+   </div>)
+         
+      
+  
    return(
       <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
+         <listItem />
          <Button />
       </>  
    );
