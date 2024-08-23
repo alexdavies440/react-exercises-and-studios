88cb4f1d234e44d11b5def0866b4c0d328bd1c2a
alexdavies440 <alexdavies440@gmail.com>
2024-08-22 23:35:35 -0500

    Studio progress...

--- part2/studio/src/components/RateARecipe.jsx
+++ part2/studio/src/components/RateARecipe.jsx
@@ -1,9 +1,13 @@
 let stars = ["⭐", "⭐ ⭐", "⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐ ⭐"];
 
-function RateARecipe() {
+function RateARecipe(props) {
+  function GiveRating(props) {
+    return <h3>{stars[props.rating -1]}</h3>
+  }
   return stars;
 }
 
 export default RateARecipe;
 
 //conditional to render stars based on a number provided in App.js 
+({props} >= 1 && {props} <= 5) ? <GiveRating props={5}/> : null 
