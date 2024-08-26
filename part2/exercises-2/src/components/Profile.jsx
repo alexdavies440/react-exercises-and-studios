893f813157689e47afc9cca0622a0c5c7e3fd7cd
alexdavies440 <alexdavies440@gmail.com>
2024-08-25 21:19:30 -0500

    Finally done

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -7,6 +7,14 @@ const listItem = oceans.map(ocean =>
    
    (<div key={ocean.id} className={`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
       <img src={ocean.image} alt={ocean.name} className="img" />
+      <h1>{ocean.name}</h1>
+      <h3>Fun Facts</h3>
+      <ul>
+         <li>{ocean.fact1}</li>
+         <li>{ocean.fact2}</li>
+         <li>{ocean.fact3}</li>
+      </ul>
+      <Button />
    </div>)
 );
 
@@ -14,12 +22,9 @@ const listItem = oceans.map(ocean =>
 function Profile() 
 {
    return (
-      <>
          <ul>
             {listItem}
          </ul>
-         <Button />
-      </>
    );
 }
 
